e5a5023123a1367e885187a877567bfd312809b6
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2024-02-18 11:45:17 -0800

    useState for object value in the state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,44 @@
 import React, { useState } from 'react'
 
 const Counter = () => {
-  const [counter, setCounter] = useState(0)
-  const [name, setName] = useState("")
-  function increaseCounter() {
-    setCounter(counter + 1)
-  }
+  // const [counter, setCounter] = useState(0)
+  // const [name, setName] = useState("")
+  // function increaseCounter() {
+  //   setCounter(counter + 1)
+  // }
   
-  return (
-        <div>
-          <input type="text" onChange={(e) => setName(e.target.value)}/ >
-          <h1>{name} has clicked Counter: {counter} times</h1>
-          <button onClick={increaseCounter}>Increase</button>
-        </div>
-      )
-    }
-    
+  // return (
+  //       <div>
+  //         <input type="text" onChange={(e) => setName(e.target.value)}/ >
+  //         <h1>{name} has clicked Counter: {counter} times</h1>
+  //         <button onClick={increaseCounter}>Increase</button>
+  //       </div>
+  //     )
+  //   }
+
+  //for obj val in state
+  //useState is function to add State to functional component
+  const [details, setDetails] = useState({counter: 0, name: " "})
+  function increaseCounter() {
+    setDetails((prev) => ({
+      ...prev,
+      counter: prev.counter + 1,
+    }))
+  }
+  function handleNameCHange(e){
+    setDetails((prev) => ({
+      ...prev, 
+      name: e.target.value
+    }))
+  }
+return (
+    <div>
+      <input type='text' onChange={handleNameCHange} />
+      <h1>
+        {details.name} has clicked {details.counter} times
+      </h1>
+      <button onClick={increaseCounter}>Increase</button>
+    </div>
+  )
+}
 export default Counter
