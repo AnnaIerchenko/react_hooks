23964e217affa387631afc5b022c1ee97182fd14
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2024-02-18 12:21:41 -0800

    useContext

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Counter from './components/Counter';
+import MainComponent from './components/MainComponent';
 import Timer from './components/Timer';
+import LoginContextProvider from './context/LoginContextProvider';
 
 function App() {
   return (
+    <LoginContextProvider>
     <div className="App">
       <Counter />
       <Timer />
+      <MainComponent />
     </div>
+    </LoginContextProvider>
   );
 }
 
